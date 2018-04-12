2f822bd52f1599a63b1219507a315af3e94bb266
arjuarsen <37340972+arjuarsen@users.noreply.github.com>
2018-04-12 19:31:22 +0530

    Update final.c

--- final.c
+++ final.c
@@ -3,7 +3,9 @@
 #include<semaphore.h>
 sem_t a;
 int c=5;
-int r1,r2,r3;
+int r1,r2,r3;1
+
+
 
 void *fun2(){
    //sem_wait(&a);
@@ -45,21 +47,27 @@ void *func(){
 
 void *fun1(){
 
- // printf("\n T1 is running in Function 1");
+ 
+  printf("\n T1 is running in Function 1");
    pthread_t T2;
    
  pthread_create(&T2,NULL,fun2,NULL);
   //sem_wait(&a);
  //r1=c;
  //r1=r1+1;
+  
+   
    pthread_create(&T2,NULL,funa,NULL);
    sleep(5);
+
  //sem_post(&a);
   //sleep(5);
   //c=r1;
   //printf("\n count = %d",c);
 //  pthread_create(&T2,NULL,func,NULL);
-      pthread_join(T2,NULL);
+   
+   
+   pthread_join(T2,NULL);
 }
 
 
@@ -67,27 +75,37 @@ void *funb(){
  sem_wait(&a);
   r2=c;
   r2=r2+1;
-  printf("\n Entered...Function B");  
+  printf("\n Entered...Function B");
+ 
  printf("\n Function B excuted before Function C");
  printf("\n Exiting...B");
- //sleep(1); 
+ sleep(5); 
   c=r2;
 printf("\n count = %d",c);
   
  sem_post(&a);
 }
 
+
 int main(){
 	
    sem_init(&a,1,1);
+
+
    pthread_t T1,T2;
    pthread_create(&T1,NULL,fun1,NULL);
    sleep(5); 
-   pthread_create(&T1,NULL,funb,NULL);    
+   pthread_create(&T1,NULL,funb,NULL);
+   sleep(5);
+   
    pthread_join(T1,NULL);
+
   pthread_create(&T2,NULL,func,NULL); 
   pthread_join(T2,NULL);
-   sem_destroy(&a);   
+   //sem_destroy(&a);   
    return 0;
   
+
+   
+
 }
